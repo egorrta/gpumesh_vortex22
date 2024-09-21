abc8428f95d0d0adeeb122b6f3ab2d3b2a1bfae8
user <egorrta@g.ucla.edu>
2024-09-21 06:16:24 +0000

    parametrized path of chipyard

--- hw/scripts/make_constellation_verilog.sh
+++ hw/scripts/make_constellation_verilog.sh
@@ -3,4 +3,4 @@
 cd $1 && \
 eval "$(conda shell.bash hook)" && \
 source ./env.sh && \
-make -C $1/sims/verilator -B $1/sims/verilator/generated-src/gpumesh.test.TestHarness.TLMesh01/sim_files.common.f SUB_PROJECT=gpumesh BINARY=none CONFIG=TLMesh01
+make -C $1/sims/verilator -B $4/sim_files.common.f SUB_PROJECT=$2 BINARY=none CONFIG=$3
